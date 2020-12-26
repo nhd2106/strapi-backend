1dd30d337e3f1ecc5a64dad9b760e4037b13176d
duoc ngo <nhd.ars@gmail.com>
2020-12-26 12:01:32 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ded66811423addff99d42f7ccc0e9117'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5e78deaa-1f8a-4511-85b6-6a29b1d1030c'
+};
